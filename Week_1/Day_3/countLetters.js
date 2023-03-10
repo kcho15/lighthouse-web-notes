6915350f3d31977f9f4800eea910c7f4d11f3177
Kevin Cho <kevcho604@gmail.com>
2023-03-10 15:20:08 -0800

    Add .md file for notes on Objects and Callback functions, small changes to backup copies to Week 1 work

--- Week_1/Day_3/countLetters.js
+++ Week_1/Day_3/countLetters.js
@@ -12,21 +12,40 @@ const assertEqual = function(actual, expected) {
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(2,2); 
 
-const countLetters = function(sentence) {
-  // function takes a string
-  let onlyLetters = sentence.replace(/ /g,'') // Delete whitespace globally 
-  let letterCount = {};
-  for (let i = 0; i < onlyLetters.length; i++) {
-    let letterKey = onlyLetters[i];  
+// const countLetters = function(sentence) {
+//   // function takes a string
+//   let onlyLetters = sentence.replace(/ /g,'') // Delete whitespace globally 
+//   let letterCount = {};
+//   for (let i = 0; i < onlyLetters.length; i++) {
+//     let letterKey = onlyLetters[i];  
         
-    if (!letterCount[letterKey]) { // If the letterKey property of letterCount is false, assign the key a value of 1
-      letterCount[letterKey] = 1;
-    } else {
-      letterCount[letterKey]++; // If the letterKey property exists, add to existing value 
+//     if (!letterCount[letterKey]) { // If the letterKey property of letterCount is false, assign the key a value of 1
+//       letterCount[letterKey] = 1;
+//     } else {
+//       letterCount[letterKey]++; // If the letterKey property exists, add to existing value 
+//     }
+//   }
+//   return letterCount;  
+// } 
+
+const countLetters = function(str) {
+  const result = {};
+
+  for (const letter of str) {
+    if (letter === ' ') {
+      continue; 
     }
+
+    // create key with each letter 
+    if (!result[letter]) {
+      result[letter] = 0; 
+    }
+    // Happy path 
+    result[letter]++ ; 
   }
-  return letterCount;  
-} 
+
+  return result; 
+}
 
 console.log(countLetters("LHL")) // expected { L: 2, H: 1 }
 console.log(countLetters("lighthouse in the house"))
